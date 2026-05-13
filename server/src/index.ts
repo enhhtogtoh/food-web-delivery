260d01c2f22480bab9fba87acaea45dacf53b48f
enhhtogtoh <enhhtogtoh@gmail.com>
2026-05-13 11:10:52 +0800

    fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,8 @@ app.use(express.json());
 
 const allowedOrigins = [
   "https://food-delivery-s682.onrender.com",
+  "https://food-web-delivery.onrender.com",
+  "https://food-web-delivery-client.vercel.app",
   "https://food-delivery-iota-five.vercel.app",
   "http://localhost:3000",   
   "http://localhost:8000"  
